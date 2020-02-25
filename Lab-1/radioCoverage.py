836385917e01a564147f86da67e85e763bbc6f6a
Doshchinsky <akter1997@gmail.com>
2020-02-25 12:39:24 +0700

    The first lab is completed

--- Lab-1/radioCoverage.py
+++ Lab-1/radioCoverage.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-def radioCoverage(arg):
-	drawgraph(x, y)
-
-def drawGraph(x, y):
-	plt.plot(x, y)
+def drawGraph(y1, y2, z1, z2, x):
+	plt.plot(x, y1, label='PathLosses 2.4 GHz')
+	plt.plot(x, y2, label='PathLosses 5 GHz')
+	plt.plot(x, z2[0], label='MAPL_UL')
+	plt.plot(x, z2[1], label='MAPL_DL')
+	plt.ylabel('PL')
+	plt.xlabel('Distance')
+	plt.legend()
 	plt.show()
-
 
 TxAP = 20
 TxUser = 20
@@ -22,22 +25,34 @@ N_AP = 5
 N_User = 8
 SINR_UL = 15
 SINR_DL = 17
-dist = 1
-bandWidth = 20
-ThermalNoise = -174 + np.log10(bandWidth)
-RxSensUser = ThermalNoise + N_User + SINR_DL
-RxSensAP = ThermalNoise + N_AP + SINR_UL
+dist_array = [1, 2, 5, 10, 15, 20, 30, 40, 50, 100]
+bandWidth = [20, 15]
+ThermalNoise = [0,0]
+ThermalNoise[0] = -174 + 10 * np.log10(bandWidth[0] * 1000000)
+ThermalNoise[1] = -174 + 10 * np.log10(bandWidth[1] * 1000000)
+RxSensUser = ThermalNoise[0] + N_User + SINR_DL
+RxSensAP = ThermalNoise[1] + N_AP + SINR_UL
 
-PathLosses = 26 * np.log10(freqRng[0]) + 22.7 + 36.7 * np.log10(dist)
+PathLosses_24 = np.zeros(10)
+PathLosses_5 = np.zeros(10)
+MAPL_array_24 = np.zeros((2, 10))
+MAPL_array_5 = np.zeros((2, 10))
+radioCoverage_24 = np.zeros((2, 1))
+radioCoverage_5 = np.zeros((2, 1))
 
-MAPL_UL = TxUser + AntG - PenLoss - IntMrg - PathLosses - RxSensUser
-MAPL_DL = TxAP + AntG - PenLoss - IntMrg - PathLosses - RxSensAP
-distUL = 10 ** ((MAPL_UL - (26 * np.log10(freqRng[0]) + 22.7)) / 36.7) 
-distDL = 10 ** ((MAPL_DL - (26 * np.log10(freqRng[0]) + 22.7)) / 36.7) 
-print(distUL)
-print(distDL)
-print(MAPL_UL)
-print(MAPL_DL)
+for cnt in range(10):
+	PathLosses_24[cnt] = 26 * np.log10(freqRng[0]) + 22.7 + 36.7 * np.log10(dist_array[cnt])
+	PathLosses_5[cnt] = 26 * np.log10(freqRng[1]) + 22.7 + 36.7 * np.log10(dist_array[cnt])
 
-#radioCoverage()
-#drawGraph()
+	MAPL_array_24[0, cnt] = TxUser + AntG - PenLoss - IntMrg - RxSensAP
+	MAPL_array_24[1, cnt] = TxAP + AntG - PenLoss - IntMrg - RxSensUser
+	MAPL_array_5[0, cnt] = TxUser + AntG - PenLoss - IntMrg - RxSensAP
+	MAPL_array_5[1, cnt] = TxAP + AntG - PenLoss - IntMrg - RxSensUser
+
+radioCoverage_24[0, 0] = 10 ** ((MAPL_array_24[0, 0] - (26 * np.log10(freqRng[0]) + 22.7)) / 36.7)
+radioCoverage_5[0, 0] = 10 ** ((MAPL_array_5[0, 0] - (26 * np.log10(freqRng[1]) + 22.7)) / 36.7)
+
+print("2.4 GHz - ", radioCoverage_24[0, 0],
+	  "\n5 GHz - ", radioCoverage_5[0, 0])
+
+drawGraph(PathLosses_24, PathLosses_5, MAPL_array_24, MAPL_array_5, dist_array)
