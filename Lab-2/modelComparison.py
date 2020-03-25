d9922abdc167e51ae06a70c0c831dd8844c9020b
Doshchinsky <akter1997@gmail.com>
2020-03-26 02:37:24 +0700

    Prepare to die edition.                                                                                 Ahpashev huyi sosi

--- Lab-2/modelComparison.py
+++ Lab-2/modelComparison.py
@@ -4,13 +4,25 @@
 # Author: 42Inc
 #
 
-
-dist = 1 				# Kilometer
+dist = 1 				# Km
 freqTrans = 900 		# MHz
+ThermalNoise = -120.989 # dB
 heightBS = 17			# Meter
 heightMS = 1.5 			# Meter
+N_AP = 2				# dB
+N_User = 7				# dB
+SIN = 7					# dB
+BPer = [22,17,12]		# dB
+SlowFading = 13			# dB
+FreqHop = 3				# dB
+meter = 2				# dB
+feeder = 3				# dB
+BodyLoss = 3			# dB
+TxPower_AP = 43			# dBm
+TxPower_User = 30		# dBm
+AntG = 17				# dBi
 
-def hataUrban():
+def PathLosses_hataUrban():
 	antCorrFact_Wiki = 3.2 * ((np.log10(11.75 * heightMS)) ** 2) - 4.97
 	antCorrFact_Lect = 3.2 * (np.log10(11.75 * heightMS)) - 4.97
 
@@ -22,7 +34,7 @@ def hataUrban():
 	 			antCorrFact_Lect + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
 	return hataU_Wiki, hataU_Lect
 
-def hataSub():
+def PathLosses_hataSub():
 	antCorrFact_Wiki = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
 	antCorrFact_Lect = 3.2 * (np.log10(11.75 * heightMS)) - 4.97
 
@@ -36,7 +48,7 @@ def hataSub():
 				2 * ((np.log10(freqTrans / 28)) ** 2) - 5.4
 	return hataS_Wiki, hataS_Lect
 
-def hataOpen():
+def PathLosses_hataOpen():
 	antCorrFact_Wiki = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
 	antCorrFact_Lect = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
 
@@ -50,7 +62,7 @@ def hataOpen():
 				4.78 * (np.log10(freqTrans) ** 2) - 18.33 * (np.log10(freqTrans)) + 40.94
 	return hataO_Wiki, hataO_Lect
 
-def costUrban():
+def PathLosses_costUrban():
 	# Wiki's formula
 	costU_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
 				(3.2 * (np.log10(11.75 * heightMS)) ** 2 - 4.97) +\
@@ -61,7 +73,7 @@ def costUrban():
 				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
 	return costU_Wiki, costU_Lect
 
-def costSub():
+def PathLosses_costSub():
 	# Wiki's formula
 	costS_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
 				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.56 * np.log10(freqTrans) - 0.8)) +\
@@ -73,7 +85,82 @@ def costSub():
 				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
 	return costS_Wiki, costS_Lect
 
-def costOpen():
+def PathLosses_costOpen():
+	# Wiki's formula
+	costO_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.56 * np.log10(freqTrans) - 0.8)) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	# Lection's formula
+	costO_Lect = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+	 			((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.5 * np.log10(freqTrans) - 0.8)) -\
+				(4.78 * (np.log10(freqTrans) ** 2) - 18.33 * (np.log10(freqTrans)) + 40.94) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	return costO_Wiki, costO_Lect
+
+def dist_hataUrban():
+	antCorrFact_Wiki = 3.2 * ((np.log10(11.75 * heightMS)) ** 2) - 4.97
+	antCorrFact_Lect = 3.2 * (np.log10(11.75 * heightMS)) - 4.97
+
+	# Wiki's formula
+	hataU_Wiki = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+				antCorrFact_Wiki + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	# Lection's formula
+	hataU_Lect = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+	 			antCorrFact_Lect + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	return hataU_Wiki, hataU_Lect
+
+def dist_hataSub():
+	antCorrFact_Wiki = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
+	antCorrFact_Lect = 3.2 * (np.log10(11.75 * heightMS)) - 4.97
+
+	# Wiki's formula
+	hataS_Wiki = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+				antCorrFact_Wiki + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				2 * ((np.log10(freqTrans / 28)) ** 2) - 5.4
+	# Lection's formula
+	hataS_Lect = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+	 			antCorrFact_Lect + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				2 * ((np.log10(freqTrans / 28)) ** 2) - 5.4
+	return hataS_Wiki, hataS_Lect
+
+def dist_hataOpen():
+	antCorrFact_Wiki = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
+	antCorrFact_Lect = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
+
+	# Wiki's formula
+	hataO_Wiki = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+				antCorrFact_Wiki + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				4.78 * (np.log10(freqTrans) ** 2) + 18.33 * (np.log10(freqTrans)) - 40.94
+	# Lection's formula
+	hataO_Lect = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+	 			antCorrFact_Lect + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				4.78 * (np.log10(freqTrans) ** 2) - 18.33 * (np.log10(freqTrans)) + 40.94
+	return hataO_Wiki, hataO_Lect
+
+def dist_costUrban():
+	# Wiki's formula
+	costU_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				(3.2 * (np.log10(11.75 * heightMS)) ** 2 - 4.97) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	# Lection's formula
+	costU_Lect = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.5 * np.log10(freqTrans) - 0.8)) - 3 +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	return costU_Wiki, costU_Lect
+
+def dist_costSub():
+	# Wiki's formula
+	costS_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.56 * np.log10(freqTrans) - 0.8)) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	# Lection's formula
+	costS_Lect = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+	 			((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.5 * np.log10(freqTrans) - 0.8)) -\
+				(2 * ((np.log10(freqTrans / 28)) ** 2) + 5.4) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	return costS_Wiki, costS_Lect
+
+def dist_costOpen():
 	# Wiki's formula
 	costO_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
 				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.56 * np.log10(freqTrans) - 0.8)) +\
@@ -89,13 +176,13 @@ def drawTable():
 	print()
 
 def main():
-	hataU_Wiki, hataU_Lect = hataUrban()	# dB
-	hataS_Wiki, hataS_Lect = hataSub()		# dB
-	hataO_Wiki, hataO_Lect = hataOpen()		# dB
+	PathLosses_hataU_Wiki, PathLosses_hataU_Lect = PathLosses_hataUrban()		# dB
+	PathLosses_hataS_Wiki, PathLosses_hataS_Lect = PathLosses_hataSub()		# dB
+	PathLosses_hataO_Wiki, PathLosses_hataO_Lect = PathLosses_hataOpen()		# dB
 
-	costU_Wiki, costU_Lect = costUrban()	# dB
-	costS_Wiki, costS_Lect = costSub()		# dB
-	costO_Wiki, costO_Lect = costOpen()		# dB
+	PathLosses_costU_Wiki, PathLosses_costU_Lect = PathLosses_costUrban()		# dB
+	PathLosses_costS_Wiki, PathLosses_costS_Lect = PathLosses_costSub()		# dB
+	PathLosses_costO_Wiki, PathLosses_costO_Lect = PathLosses_costOpen()		# dB
 
 	print("Path Losses: Okumura Hata in Urban Env")
 	print("Wiki:", hataU_Wiki, "\tLection:", hataU_Lect,"\n")
@@ -110,6 +197,31 @@ def main():
 	print("Wiki:", costS_Wiki, "\tLection:", costS_Lect,"\n")
 	print("Path Losses: COST Hata in Open Env")
 	print("Wiki:", costO_Wiki, "\tLection:", costO_Lect,"\n")
+
+	RxSenseUser = N_User + ThermalNoise + SIN
+	RxSenseAP = N_AP + ThermalNoise + SIN
+
+	MAPL_DL_Urban = TxPower_AP + AntG - feeder - RxSenseUser - SlowFading - FreqHop - BodyLoss - BPer[0]
+	MAPL_UL_Urban = TxPower_User + AntG - feeder - RxSenseAP - SlowFading - FreqHop - BodyLoss - BPer[0]
+	MAPL_DL_SubUrban = TxPower_AP + AntG - feeder - RxSenseUser - SlowFading - FreqHop - BodyLoss - BPer[1]
+	MAPL_UL_SubUrban = TxPower_User + AntG - feeder - RxSenseAP - SlowFading - FreqHop - BodyLoss - BPer[1]
+	MAPL_DL_Open = TxPower_AP + AntG - feeder - RxSenseUser - SlowFading - FreqHop - BodyLoss - BPer[2]
+	MAPL_UL_Open = TxPower_User + AntG - feeder - RxSenseAP - SlowFading - FreqHop - BodyLoss - BPer[2]
+
+	print("Path Losses: MAPL in Urban Env")
+	print("Uplink:", MAPL_UL_Urban, "\tDowlink:", MAPL_DL_Urban,"\n")
+	print("Path Losses: MAPL in Suburban Env")
+	print("Uplink:", MAPL_UL_SubUrban, "\tDowlink:", MAPL_DL_SubUrban,"\n")
+	print("Path Losses: MAPL in Open Env")
+	print("Uplink:", MAPL_UL_Open, "\tDowlink:", MAPL_DL_Open,"\n")
+
+	dist_hataU_Wiki, dist_hataU_Lect = dist_hataUrban()		# Km
+	dist_hataS_Wiki, dist_hataS_Lect = dist_hataSub()		# Km
+	dist_hataO_Wiki, dist_hataO_Lect = dist_hataOpen()		# Km
+
+	dist_costU_Wiki, dist_costU_Lect = dist_costUrban()		# Km
+	dist_costS_Wiki, dist_costS_Lect = dist_costSub()		# Km
+	dist_costO_Wiki, dist_costO_Lect = dist_costOpen()		# Km
 
 	drawTable()
 
