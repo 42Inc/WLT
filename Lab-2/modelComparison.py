ec257a832544ad89c7a7fa0642082dea0856d398
Doshchinsky <akter1997@gmail.com>
2020-03-26 04:52:29 +0700

    Added square counting and now we know - lection sucks, there is a tonn of mistakes... Now I need to add beautiful tables for it

--- Lab-2/modelComparison.py
+++ Lab-2/modelComparison.py
@@ -171,35 +171,55 @@ def dist_costOpen(costO_Wiki):
 				(44.9 - 6.55 * np.log10(heightBS)))
 	return dist, dist2
 
+def radioCoverage2G(dist_hataU_Wiki, dist_hataU_Lect, dist_hataS_Wiki, dist_hataS_Lect,
+					dist_hataO_Wiki, dist_hataO_Lect, dist_costU_Wiki, dist_costU_Lect,
+					dist_costS_Wiki, dist_costS_Lect, dist_costO_Wiki, dist_costO_Lect):
+	area_hataU_Wiki = (dist_hataU_Wiki ** 2 - (heightBS / 1000) ** 2) * pi
+	area_hataU_Lect = (dist_hataU_Lect ** 2 - (heightBS / 1000) ** 2) * pi
+	area_hataS_Wiki = (dist_hataS_Wiki ** 2 - (heightBS / 1000) ** 2) * pi
+	area_hataS_Lect = (dist_hataS_Lect ** 2 - (heightBS / 1000) ** 2) * pi
+	area_hataO_Wiki = (dist_hataO_Wiki ** 2 - (heightBS / 1000) ** 2) * pi
+	area_hataO_Lect = (dist_hataO_Lect ** 2 - (heightBS / 1000) ** 2) * pi
+	area_costU_Wiki = (dist_costU_Wiki ** 2 - (heightBS / 1000) ** 2) * pi
+	area_costU_Lect = (dist_costU_Lect ** 2 - (heightBS / 1000) ** 2) * pi
+	area_costS_Wiki = (dist_costS_Wiki ** 2 - (heightBS / 1000) ** 2) * pi
+	area_costS_Lect = (dist_costS_Lect ** 2 - (heightBS / 1000) ** 2) * pi
+	area_costO_Wiki = (dist_costO_Wiki ** 2 - (heightBS / 1000) ** 2) * pi
+	area_costO_Lect = (dist_costO_Lect ** 2 - (heightBS / 1000) ** 2) * pi
+
+	return area_hataU_Wiki, area_hataU_Lect, area_hataS_Wiki, area_hataS_Lect, area_hataO_Wiki, area_hataO_Lect, area_costU_Wiki, area_costU_Lect, area_costS_Wiki, area_costS_Lect, area_costO_Wiki, area_costO_Lect
+
 def drawTable():
 	print()
 
 def main():
+	# Counting path losses with both formulas: Wiki and lection
 	PathLosses_hataU_Wiki, PathLosses_hataU_Lect = PathLosses_hataUrban()		# dB
-	PathLosses_hataS_Wiki, PathLosses_hataS_Lect = PathLosses_hataSub()		# dB
+	PathLosses_hataS_Wiki, PathLosses_hataS_Lect = PathLosses_hataSub()			# dB
 	PathLosses_hataO_Wiki, PathLosses_hataO_Lect = PathLosses_hataOpen()		# dB
 
 	PathLosses_costU_Wiki, PathLosses_costU_Lect = PathLosses_costUrban()		# dB
-	PathLosses_costS_Wiki, PathLosses_costS_Lect = PathLosses_costSub()		# dB
+	PathLosses_costS_Wiki, PathLosses_costS_Lect = PathLosses_costSub()			# dB
 	PathLosses_costO_Wiki, PathLosses_costO_Lect = PathLosses_costOpen()		# dB
 
 	print("Path Losses: Okumura Hata in Urban Env")
-	print("Wiki:", PathLosses_hataU_Wiki, "\tLection:", PathLosses_hataU_Lect,"\n")
+	print("Wiki:", PathLosses_hataU_Wiki, "\tLection:", PathLosses_hataU_Lect)
 	print("Path Losses: Okumura Hata in Suburban Env")
-	print("Wiki:", PathLosses_hataS_Wiki, "\tLection:", PathLosses_hataS_Lect,"\n")
+	print("Wiki:", PathLosses_hataS_Wiki, "\tLection:", PathLosses_hataS_Lect)
 	print("Path Losses: Okumura Hata in Open Env")
-	print("Wiki:", PathLosses_hataO_Wiki, "\tLection:", PathLosses_hataO_Lect,"\n")
+	print("Wiki:", PathLosses_hataO_Wiki, "\tLection:", PathLosses_hataO_Lect)
 
 	print("Path Losses: COST Hata in Urban Env")
-	print("Wiki:", PathLosses_costU_Wiki, "\tLection:", PathLosses_costU_Lect,"\n")
+	print("Wiki:", PathLosses_costU_Wiki, "\tLection:", PathLosses_costU_Lect)
 	print("Path Losses: COST Hata in Suburban Env")
-	print("Wiki:", PathLosses_costS_Wiki, "\tLection:", PathLosses_costS_Lect,"\n")
+	print("Wiki:", PathLosses_costS_Wiki, "\tLection:", PathLosses_costS_Lect)
 	print("Path Losses: COST Hata in Open Env")
 	print("Wiki:", PathLosses_costO_Wiki, "\tLection:", PathLosses_costO_Lect,"\n")
 
 	RxSenseUser = N_User + ThermalNoise + SIN
 	RxSenseAP = N_AP + ThermalNoise + SIN
 
+	# Counting MAPL Uplink and Downlink
 	MAPL_DL_Urban = TxPower_AP + AntG - feeder - RxSenseUser - SlowFading - FreqHop - BodyLoss - BPer[0]
 	MAPL_UL_Urban = TxPower_User + AntG - feeder - RxSenseAP - SlowFading - FreqHop - BodyLoss - BPer[0]
 	MAPL_DL_SubUrban = TxPower_AP + AntG - feeder - RxSenseUser - SlowFading - FreqHop - BodyLoss - BPer[1]
@@ -208,33 +228,56 @@ def main():
 	MAPL_UL_Open = TxPower_User + AntG - feeder - RxSenseAP - SlowFading - FreqHop - BodyLoss - BPer[2]
 
 	print("Path Losses: MAPL in Urban Env")
-	print("Uplink:", MAPL_UL_Urban, "\tDowlink:", MAPL_DL_Urban,"\n")
+	print("Uplink:", MAPL_UL_Urban, "\tDowlink:", MAPL_DL_Urban)
 	print("Path Losses: MAPL in Suburban Env")
-	print("Uplink:", MAPL_UL_SubUrban, "\tDowlink:", MAPL_DL_SubUrban,"\n")
+	print("Uplink:", MAPL_UL_SubUrban, "\tDowlink:", MAPL_DL_SubUrban)
 	print("Path Losses: MAPL in Open Env")
 	print("Uplink:", MAPL_UL_Open, "\tDowlink:", MAPL_DL_Open,"\n")
 
+	# Counting radiuses of MAPL distances
 	dist_hataU_Wiki, dist_hataU_Lect = dist_hataUrban(MAPL_UL_Urban)		# Km
 	dist_hataS_Wiki, dist_hataS_Lect = dist_hataSub(MAPL_UL_SubUrban)		# Km
-	dist_hataO_Wiki, dist_hataO_Lect = dist_hataOpen(MAPL_UL_Open)		# Km
+	dist_hataO_Wiki, dist_hataO_Lect = dist_hataOpen(MAPL_UL_Open)			# Km
 
 	dist_costU_Wiki, dist_costU_Lect = dist_costUrban(MAPL_UL_Urban)		# Km
 	dist_costS_Wiki, dist_costS_Lect = dist_costSub(MAPL_UL_SubUrban)		# Km
 	dist_costO_Wiki, dist_costO_Lect = dist_costOpen(MAPL_UL_Open)			# Km
 
-	print("Path Losses: Okumura Hata in Urban Env")
-	print("Wiki:", dist_hataU_Wiki, "\tLection:", dist_hataU_Lect,"\n")
-	print("Path Losses: Okumura Hata in Suburban Env")
-	print("Wiki:", dist_hataS_Wiki, "\tLection:", dist_hataS_Lect,"\n")
-	print("Path Losses: Okumura Hata in Open Env")
-	print("Wiki:", dist_hataO_Wiki, "\tLection:", dist_hataO_Lect,"\n")
+	print("Radius: Okumura Hata in Urban Env")
+	print("Wiki:", dist_hataU_Wiki, "\tLection:", dist_hataU_Lect)
+	print("Radius: Okumura Hata in Suburban Env")
+	print("Wiki:", dist_hataS_Wiki, "\tLection:", dist_hataS_Lect)
+	print("Radius: Okumura Hata in Open Env")
+	print("Wiki:", dist_hataO_Wiki, "\tLection:", dist_hataO_Lect)
 
-	print("Path Losses: COST Hata in Urban Env")
-	print("Wiki:", dist_costU_Wiki, "\tLection:", dist_costU_Lect,"\n")
-	print("Path Losses: COST Hata in Suburban Env")
-	print("Wiki:", dist_costS_Wiki, "\tLection:", dist_costS_Lect,"\n")
-	print("Path Losses: COST Hata in Open Env")
+	print("Radius: COST Hata in Urban Env")
+	print("Wiki:", dist_costU_Wiki, "\tLection:", dist_costU_Lect)
+	print("Radius: COST Hata in Suburban Env")
+	print("Wiki:", dist_costS_Wiki, "\tLection:", dist_costS_Lect)
+	print("Radius: COST Hata in Open Env")
 	print("Wiki:", dist_costO_Wiki, "\tLection:", dist_costO_Lect,"\n")
+
+	# Counting radio coverage for 2G networks with both formulas
+	radioCoverageArr = [ 0.0 for cnt in range(12)]
+	radioCoverageArr = radioCoverage2G(dist_hataU_Wiki, dist_hataU_Lect,
+										dist_hataS_Wiki, dist_hataS_Lect,
+										dist_hataO_Wiki, dist_hataO_Lect,
+										dist_costU_Wiki, dist_costU_Lect,
+										dist_costS_Wiki, dist_costS_Lect,
+										dist_costO_Wiki, dist_costO_Lect)
+	print("Area: Okumura Hata in Urban Env")
+	print("Wiki:", radioCoverageArr[0], "\tLection:", radioCoverageArr[1])
+	print("Area: Okumura Hata in Suburban Env")
+	print("Wiki:", radioCoverageArr[2], "\tLection:", radioCoverageArr[3])
+	print("Area: Okumura Hata in Open Env")
+	print("Wiki:", radioCoverageArr[4], "\tLection:", radioCoverageArr[5])
+
+	print("Area: COST Hata in Urban Env")
+	print("Wiki:", radioCoverageArr[6], "\tLection:", radioCoverageArr[7])
+	print("Area: COST Hata in Suburban Env")
+	print("Wiki:", radioCoverageArr[8], "\tLection:", radioCoverageArr[9])
+	print("Area: COST Hata in Open Env")
+	print("Wiki:", radioCoverageArr[10], "\tLection:", radioCoverageArr[11])
 
 	drawTable()
 
@@ -242,6 +285,7 @@ if __name__ == "__main__":
 	try:
 		import sys
 		import numpy as np
+		from math import pi
 		import texttable as tt
 	except Exception as err:
 		print(f"Error while loading dependencies:", err)
