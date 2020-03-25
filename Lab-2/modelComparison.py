5eea19ec5716c1bfceee7346612cb39d966c5d2d
Doshchinsky <akter1997@gmail.com>
2020-03-26 00:28:43 +0700

    Added the implementation of Okumura Hata models (Urban, Suburban and Open areas) with comparisons of Wiki's and lection's formulas

--- Lab-2/modelComparison.py
+++ Lab-2/modelComparison.py
@@ -1,53 +1,91 @@
 #!/usr/bin/env python3
 
+#
 # Author: 42Inc
+#
 
-global __hataU__
-global __hataS__
-global __hataO__
 
-global __costU__
-global __costS__
-global __costO__
+dist = 1 				# Kilometer
+freqTrans = 900 		# MHz
+heightBS = 17			# Meter
+heightMS = 1.5 			# Meter
 
 def hataUrban():
-	print("Okay")
+	antCorrFact_Wiki = 3.2 * ((np.log10(11.75 * heightMS)) ** 2) - 4.97
+	antCorrFact_Lect = 3.2 * (np.log10(11.75 * heightMS)) - 4.97
+
+	# Wiki's formula
+	hataU_Wiki = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+				antCorrFact_Wiki + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	# Lection's formula
+	hataU_Lect = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+	 			antCorrFact_Lect + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	return hataU_Wiki, hataU_Lect
 
 def hataSub():
-	print("Okay")
+	antCorrFact_Wiki = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
+	antCorrFact_Lect = 3.2 * (np.log10(11.75 * heightMS)) - 4.97
+
+	# Wiki's formula
+	hataS_Wiki = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+				antCorrFact_Wiki + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				2 * ((np.log10(freqTrans / 28)) ** 2) - 5.4
+	# Lection's formula
+	hataS_Lect = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+	 			antCorrFact_Lect + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				2 * ((np.log10(freqTrans / 28)) ** 2) - 5.4
+	return hataS_Wiki, hataS_Lect
 
 def hataOpen():
-	print("Okay")
+	antCorrFact_Wiki = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
+	antCorrFact_Lect = 0.8 + (1.1 * np.log10(freqTrans) - 0.7) * heightMS - 1.56 * np.log10(freqTrans)
+
+	# Wiki's formula
+	hataO_Wiki = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+				antCorrFact_Wiki + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				4.78 * (np.log10(freqTrans) ** 2) + 18.33 * (np.log10(freqTrans)) - 40.94
+	# Lection's formula
+	hataO_Lect = 69.55 + 26.66 * np.log10(freqTrans) - 13.82 * np.log10(heightBS) -\
+	 			antCorrFact_Lect + (44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) -\
+				4.78 * (np.log10(freqTrans) ** 2) - 18.33 * (np.log10(freqTrans)) + 40.94
+	return hataO_Wiki, hataO_Lect
 
 def costUrban():
-	print("Okay")
+	print()
 
 def costSub():
-	print("Okay")
+	print()
 
 def costOpen():
-	print("Okay")
+	print()
 
 def drawTable():
-	print("Okay")
+	print()
 
 def main():
-	hataUrban()
-	hataSub()
-	hataOpen()
+	hataU_Wiki, hataU_Lect = hataUrban()	# dB
+	hataS_Wiki, hataS_Lect = hataSub()		# dB
+	hataO_Wiki, hataO_Lect = hataOpen()
 
 	costUrban()
 	costSub()
 	costOpen()
+
+	print("Path Losses: Okumura Hata in Urban Env")
+	print("Wiki:", hataU_Wiki, "\tLection:", hataU_Lect,"\n")
+	print("Path Losses: Okumura Hata in Suburban Env")
+	print("Wiki:", hataS_Wiki, "\tLection:", hataS_Lect,"\n")
+	print("Path Losses: Okumura Hata in Open Env")
+	print("Wiki:", hataO_Wiki, "\tLection:", hataO_Lect,"\n")
 
 	drawTable()
 
 if __name__ == "__main__":
 	try:
 		import sys
-		import numpy
-		import texttable
+		import numpy as np
+		import texttable as tt
 	except Exception as err:
 		print(f"Error while loading dependencies:", err)
 		exit(-1)
-	sys.exit(main())
+	main()
