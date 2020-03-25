1d9ed924b84fb5eb16a3a1e303bc7c9284a80890
Doshchinsky <akter1997@gmail.com>
2020-03-26 01:02:40 +0700

    Added the implementation of COST Hata models (Urban, Suburban and Open areas) with comparisons of Wiki's and lection's formulas

--- Lab-2/modelComparison.py
+++ Lab-2/modelComparison.py
@@ -51,13 +51,39 @@ def hataOpen():
 	return hataO_Wiki, hataO_Lect
 
 def costUrban():
-	print()
+	# Wiki's formula
+	costU_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				(3.2 * (np.log10(11.75 * heightMS)) ** 2 - 4.97) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	# Lection's formula
+	costU_Lect = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.5 * np.log10(freqTrans) - 0.8)) - 3 +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	return costU_Wiki, costU_Lect
 
 def costSub():
-	print()
+	# Wiki's formula
+	costS_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.56 * np.log10(freqTrans) - 0.8)) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	# Lection's formula
+	costS_Lect = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+	 			((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.5 * np.log10(freqTrans) - 0.8)) -\
+				(2 * ((np.log10(freqTrans / 28)) ** 2) + 5.4) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	return costS_Wiki, costS_Lect
 
 def costOpen():
-	print()
+	# Wiki's formula
+	costO_Wiki = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+				((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.56 * np.log10(freqTrans) - 0.8)) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist)
+	# Lection's formula
+	costO_Lect = 46.3 + 33.9 * (np.log10(freqTrans)) - 13.82 * (np.log10(heightBS)) -\
+	 			((1.1 * np.log10(freqTrans) - 0.7) * heightMS - (1.5 * np.log10(freqTrans) - 0.8)) -\
+				(4.78 * (np.log10(freqTrans) ** 2) - 18.33 * (np.log10(freqTrans)) + 40.94) +\
+				(44.9 - 6.55 * np.log10(heightBS)) * np.log10(dist) + 3
+	return costO_Wiki, costO_Lect
 
 def drawTable():
 	print()
@@ -65,11 +91,11 @@ def drawTable():
 def main():
 	hataU_Wiki, hataU_Lect = hataUrban()	# dB
 	hataS_Wiki, hataS_Lect = hataSub()		# dB
-	hataO_Wiki, hataO_Lect = hataOpen()
+	hataO_Wiki, hataO_Lect = hataOpen()		# dB
 
-	costUrban()
-	costSub()
-	costOpen()
+	costU_Wiki, costU_Lect = costUrban()	# dB
+	costS_Wiki, costS_Lect = costSub()		# dB
+	costO_Wiki, costO_Lect = costOpen()		# dB
 
 	print("Path Losses: Okumura Hata in Urban Env")
 	print("Wiki:", hataU_Wiki, "\tLection:", hataU_Lect,"\n")
@@ -77,6 +103,13 @@ def main():
 	print("Wiki:", hataS_Wiki, "\tLection:", hataS_Lect,"\n")
 	print("Path Losses: Okumura Hata in Open Env")
 	print("Wiki:", hataO_Wiki, "\tLection:", hataO_Lect,"\n")
+
+	print("Path Losses: COST Hata in Urban Env")
+	print("Wiki:", costU_Wiki, "\tLection:", costU_Lect,"\n")
+	print("Path Losses: COST Hata in Suburban Env")
+	print("Wiki:", costS_Wiki, "\tLection:", costS_Lect,"\n")
+	print("Path Losses: COST Hata in Open Env")
+	print("Wiki:", costO_Wiki, "\tLection:", costO_Lect,"\n")
 
 	drawTable()
 
