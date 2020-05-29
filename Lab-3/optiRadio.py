91b0f29ca99474623663bd0b740da329912fe565
Doshchinsky <akter1997@gmail.com>
2020-05-29 19:46:45 +0100

    Ta ebannyyi v rot etogo casino

--- Lab-3/optiRadio.py
+++ Lab-3/optiRadio.py
@@ -9,17 +9,34 @@ B = [-100,100]
 
 dist = [1, 2, 3, 4, 5, 6]
 TxAP = 16			# dBm
-TxUser =			# dBm
+TxUser = 20			# dBm
 channel = 6
 FreqAP = 2.437		# GHz
 RxSenseAP = 90		# dBm
-
+expCount = 100
 Pexp = []
 Ptheo = []
 
-def optiFunc():
-	X(d) = 	26 * np.log10(FreqAP) + 22.7 + 36.7 * np.log10(dist)
-	L(d) = A * X(d) + B
+def HalfLife3(inter):
+	leftBorder = inter[0]
+	rightBorder = inter[1]
+	while (rightBorder - leftBorder > 1):
+		length = rightBorder - leftBorder
+		halflife = int(length / 2)
+		mid = leftBorder + halflife
+		if (leftBorder >= 0) and (mid < 0):
+			rightBorder = mid
+		else:
+			leftBorder = mid
+	return mid
+
+def optiFunc(Amin, Bmin):
+	L = [ 0.0 for cnt in range(len(dist))]
+	for iter in range(len(dist)):
+		PathLosses = 26 * np.log10(FreqAP) + 22.7 + 36.7 * np.log10(dist[iter])
+		L[iter] = Amin * PathLosses + Bmin
+		print("Distance =", dist[iter], "\tPathLosses =", PathLosses, "\tL =", L[iter])
+	return L
 
 def errorRate():
 	for iter in range(expCount):
@@ -27,12 +44,15 @@ def errorRate():
 	return err
 
 def main():
-	res = optiFunc()
-	err = errorRate()
+	Amin = HalfLife3(A)
+	Bmin = HalfLife3(B)
+	res = optiFunc(Amin, Bmin)
+	# err = errorRate()
 
 if __name__ == '__main__':
 	try:
 		import sys
+		from time import sleep
 		import numpy as np
 		from math import pi
 	except Exception as e:
