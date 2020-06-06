e21f291560ea8ade770309c8525df44ae24a8d09
Alexey R <alexey_r.98@ngs.ru>
2020-06-06 20:14:38 +0700

    Its temporary splution. Need to check it

--- Lab-3/optiRadio.py
+++ Lab-3/optiRadio.py
@@ -7,46 +7,56 @@
 A = [-10,10]
 B = [-100,100]
 
-dist = [1, 2, 3, 4, 5, 6]
+dist = 15
 TxAP = 16			# dBm
 TxUser = 20			# dBm
 channel = 6
 FreqAP = 2.437		# GHz
 RxSenseAP = 90		# dBm
 expCount = 100
-Pexp = []
-Ptheo = []
+Pexp = -86
+Ptheo = 0
 
-def HalfLife3(inter):
-	leftBorder = inter[0]
-	rightBorder = inter[1]
-	while (rightBorder - leftBorder > 1):
-		length = rightBorder - leftBorder
-		halflife = int(length / 2)
-		mid = leftBorder + halflife
-		if (leftBorder >= 0) and (mid < 0):
-			rightBorder = mid
-		else:
-			leftBorder = mid
-	return mid
+# def HalfLife3(inter):
+# 	leftBorder = inter[0]
+# 	rightBorder = inter[1]
+# 	while (rightBorder - leftBorder > 1):
+# 		length = rightBorder - leftBorder
+# 		halflife = int(length / 2)
+# 		mid = leftBorder + halflife
+# 		if (leftBorder >= 0) and (mid < 0):
+# 			rightBorder = mid
+# 		else:
+# 			leftBorder = mid
+# 	return mid
 
-def optiFunc(Amin, Bmin):
-	L = [ 0.0 for cnt in range(len(dist))]
-	for iter in range(len(dist)):
-		PathLosses = 26 * np.log10(FreqAP) + 22.7 + 36.7 * np.log10(dist[iter])
-		L[iter] = Amin * PathLosses + Bmin
-		print("Distance =", dist[iter], "\tPathLosses =", PathLosses, "\tL =", L[iter])
-	return L
+def optiFunc(Ad, Bd):
+	minO = 100000000
+	minA = 0
+	minB = 0
+	# L = [ 0.0 for cnt in range(len(dist))]
+	for iter1 in range(Ad[1] - Ad[0]):
+		for iter2 in range(Bd[1] - Bd[0]):
+			PathLosses = 26 * np.log10(FreqAP) + 22.7 + 36.7 * np.log10(dist)
+			Ptheo = (iter1 + Ad[0]) * PathLosses + (iter2 + Bd[0])
+			# print("Distance =", dist, "\tPathLosses =", Ptheo, "\tPexp =", Pexp, "\tminA =", (iter1 + Ad[0]), "\tminB =", (iter2 + Bd[0]) )
+			if minO > (Ptheo - Pexp) ** 2 and (iter1 + Ad[0]) != 0:
+				minO =  (Ptheo - Pexp) ** 2
+				minA = (iter1 + Ad[0])
+				minB = (iter2 + Bd[0])
+	PathLosses = 26 * np.log10(FreqAP) + 22.7 + 36.7 * np.log10(dist)
+	Ptheo = float(minA) * float(PathLosses) + minB
+	print("Distance =", dist, "\tPathLosses =", Ptheo, "\tPexp =", Pexp, "\tminA =", minA, "\tminB =", minB)
 
-def errorRate():
-	for iter in range(expCount):
-		err += (Ptheo - Pexp) ** 2
-	return err
+# def errorRate():
+# 	for iter in range(expCount):
+# 		err += (Ptheo - Pexp) ** 2
+# 	return err
 
 def main():
-	Amin = HalfLife3(A)
-	Bmin = HalfLife3(B)
-	res = optiFunc(Amin, Bmin)
+	# Amin = HalfLife3(A)
+	# Bmin = HalfLife3(B)
+	optiFunc(A, B)
 	# err = errorRate()
 
 if __name__ == '__main__':
